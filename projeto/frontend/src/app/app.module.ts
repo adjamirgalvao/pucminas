88be1bae22e7032a7b36eb2aaea60ce7aba1be9d
adjamirgalvao <adjamir@gmail.com>
2023-01-06 18:48:37 -0300

    criarProduto com ReactiveForms

--- projeto/frontend/src/app/app.module.ts
+++ projeto/frontend/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -18,7 +19,8 @@ import { HomeComponent } from './componentes/home/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- projeto/frontend/src/app/componentes/produtos/criar-produto/criar-produto.component.ts
+++ projeto/frontend/src/app/componentes/produtos/criar-produto/criar-produto.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-criar-produto',
   templateUrl: './criar-produto.component.html',
   styleUrls: ['./criar-produto.component.css']
 })
+
 export class CriarProdutoComponent {
+
+  criarProdutoForm = this.formBuilder.group({
+    nome: '',
+    estoque: null,
+    preco: null
+  });
+
+
+  constructor(
+    private formBuilder: FormBuilder){}
+
+  criarProduto(): void {
+    // Criação do produto
+    console.warn('Your order has been submitted', this.criarProdutoForm.value);
+    this.criarProdutoForm.reset();
+  }
 
 }
