658f1a3cd12ab32bb46e330a84a43c472fe5b622
adjamirgalvao <adjamir@gmail.com>
2023-02-07 00:23:44 -0300

    Fazendo a parte de autorização no backend, corrigindo erro para 403 e ajustando rotas.

--- projeto/backend/controllers/UsuarioController.js
+++ projeto/backend/controllers/UsuarioController.js
@@ -1,101 +1,93 @@
 const UsuarioService = require("../services/UsuarioService");
-const jwt = require('jsonwebtoken');
-const Config = require('../config/config');
+const { AutorizacaoService, ROLES } = require("../services/AutorizacaoService");
 
 exports.get = async (req, res) => {
   let id = req.params.id;
 
-  try {
-    const registro = await UsuarioService.getUsuariobyId(id);
-    res.json(registro);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+  if (AutorizacaoService.isMesmoUsuario(req, id) || AutorizacaoService.validarRoles(req, [ROLES.ADMIN, ROLES.MASTER])) {
+    try {
+      const registro = await UsuarioService.getUsuariobyId(id);
+      res.json(registro);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.status(403).json({ error: 'Acesso negado' });
   }
 };
 
 exports.getAll = async (req, res) => {
-  // https://stackoverflow.com/questions/9216185/nodejs-passport-display-username
-  console.log('username', req.user);
-  try {
-    const registros = await UsuarioService.getAllUsuarios();
+  if (AutorizacaoService.validarRoles(req, [ROLES.ADMIN, ROLES.MASTER])) {
+    try {
+      const registros = await UsuarioService.getAllUsuarios();
 
-    if (!registros) {
-      return res.status(404).json("Não existem usuarios cadastrados!");
+      if (!registros) {
+        return res.status(404).json("Não existem usuarios cadastrados!");
+      }
+
+      res.json(registros);
+    } catch (err) {
+      return res.status(500).json({ error: err.message });
     }
-
-    res.json(registros);
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
+  } else {
+    res.status(403).json({ error: 'Acesso negado' });
   }
 };
 
 exports.add = async (req, res) => {
-  try {
-    const registro = await UsuarioService.addUsuario(req.body);
-    res.status(201).json(registro);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+  if (AutorizacaoService.isNovoUsuarioCliente(req.body) || AutorizacaoService.validarRoles(req, [ROLES.ADMIN, ROLES.MASTER])) {
+    try {
+      const registro = await UsuarioService.addUsuario(req.body);
+      res.status(201).json(registro);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.status(403).json({ error: 'Acesso negado' });
   }
 };
 
 exports.update = async (req, res) => {
   let id = req.params.id;
 
-  try {
-    const usuario =  {
-      nome: req.body.nome,
-      login: req.body.cpf,
-      email: req.body.email,
-      senha: req.body.senha,
-      roles: req.body.roles
-  };
+  if (AutorizacaoService.isMesmoUsuario(req, id) || AutorizacaoService.validarRoles(req, [ROLES.ADMIN, ROLES.MASTER])) {
+    try {
+      const usuario = {
+        nome: req.body.nome,
+        login: req.body.cpf,
+        email: req.body.email,
+        senha: req.body.senha,
+        roles: req.body.roles
+      };
 
-    console.log(usuario, id);
-    const registro = await UsuarioService.updateUsuario(id, usuario);
+      console.log(usuario, id);
+      const registro = await UsuarioService.updateUsuario(id, usuario);
 
-    if (registro.nModified === 0) {
-      return res.status(404).json({});
+      if (registro.nModified === 0) {
+        return res.status(404).json({});
+      }
+
+      res.json(registro);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
-
-    res.json(registro);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+  } else {
+    res.status(403).json({ error: 'Acesso negado' });
   }
 };
 
 exports.delete = async (req, res) => {
-  let id = req.params.id;
+  if (AutorizacaoService.validarRoles(req, [ROLES.ADMIN, ROLES.MASTER])) {
+    let id = req.params.id;
 
-  try {
-    const registro = await UsuarioService.deleteUsuario(id);
-    res.json(registro);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-exports.login = async (req, res) => {
-  try {
-    let {login, senha} = req.body;
-    const usuarios = await UsuarioService.find({login: login});
-
-    if (usuarios && (usuarios.length == 1) && (usuarios[0].senha == senha)) {
-          // Sign token
-          const token = jwt.sign({ login }, Config.passport.secret, {
-            expiresIn: Config.passport.expiresIn,
-          });
-
-          //https://stackoverflow.com/questions/23342558/why-cant-i-delete-a-mongoose-models-object-properties
-          let retorno = {usuario: usuarios[0].toObject()};
-          retorno.token = token;
-          delete retorno.usuario.senha;
-          delete retorno.usuario._id;
-          res.status(200).json(retorno);
-    } else {
-      res.status(404).json({error: 'Usuario não encontrado'});      
+    try {
+      const registro = await UsuarioService.deleteUsuario(id);
+      res.json(registro);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+  } else {
+    res.status(403).json({ error: 'Acesso negado' });
   }
-
 };
+
