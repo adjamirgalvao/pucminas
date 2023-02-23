56422ba9762905ce4142d680b8264eb29c45f453
adjamirgalvao <adjamir@gmail.com>
2023-02-23 10:40:23 -0300

    melhorando exibição de clientes no mobile

--- projeto/frontend/src/app/componentes/clientes/listar-clientes/listar-clientes.component.ts
+++ projeto/frontend/src/app/componentes/clientes/listar-clientes/listar-clientes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -15,7 +15,7 @@ import { ModalConfirmacaoComponent } from '../../util/modal-confirmacao/modal-co
   templateUrl: './listar-clientes.component.html',
   styleUrls: ['./listar-clientes.component.css']
 })
-export class ListarClientesComponent implements OnInit {
+export class ListarClientesComponent implements OnInit, OnDestroy {
 
   constructor(
     private route: ActivatedRoute,
@@ -30,6 +30,35 @@ export class ListarClientesComponent implements OnInit {
       }
   }
 
+  
+  handlerOrientation: any;
+  
+  ngOnDestroy(): void {
+    console.log('on destroy');
+    //https://stackoverflow.com/questions/46906763/how-to-remove-eventlisteners-in-angular-4
+    //https://code.daypilot.org/79036/angular-calendar-detect-orientation-change-landscape-portrait
+    this.landscape.removeEventListener("change", this.handlerOrientation, true);
+  }
+
+  private onChangeOrientation() {
+    console.log('landscape orientation');
+  }
+
+  isPortrait() {
+    return !this.landscape.matches;
+  }   
+   
+  //https://stackoverflow.com/questions/47077302/angular2-material-table-hide-column
+  //https://stackoverflow.com/questions/41432533/how-to-detect-if-device-is-desktop-and-or-mobile-and-if-connection-is-wifi-or-n
+  getDisplayedColumns() : string[] {
+    let exibir = !this.isPortrait();
+    return this.displayedColumns.filter(cd => exibir || cd.showMobile).map(cd => cd.def);
+  } 
+
+  //https://code.daypilot.org/79036/angular-calendar-detect-orientation-change-landscape-portrait
+  landscape = window.matchMedia("(orientation: landscape)");  
+
+
   alertas: Alerta[] = [];
   clientes: Cliente[] = [];
   carregando: boolean = true;
@@ -39,7 +68,7 @@ export class ListarClientesComponent implements OnInit {
   clienteExcluido!: Cliente;
 
   // Campos para a tabela
-  displayedColumns: string[] = ['nome', 'cpf', 'dataNascimento', 'acoes'];
+  displayedColumns = [{ def: 'nome', showMobile: true}, {def: 'cpf', showMobile: true}, {def: 'dataNascimento', showMobile: false}, {def: 'acoes', showMobile: true}];
   dataSource: MatTableDataSource<Cliente> = new MatTableDataSource();
 
   //Sem isso não consegui fazer funcionar o sort e paginator https://stackoverflow.com/questions/50767580/mat-filtering-mat-sort-not-work-correctly-when-use-ngif-in-mat-table-parent  
@@ -62,6 +91,9 @@ export class ListarClientesComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    //https://code.daypilot.org/79036/angular-calendar-detect-orientation-change-landscape-portrait
+    this.handlerOrientation = this.onChangeOrientation.bind(this);
+    this.landscape.addEventListener("change", this.handlerOrientation, true);        
     //Recuperando os dados
     this.clienteService.listar().pipe(catchError(
       err => {
